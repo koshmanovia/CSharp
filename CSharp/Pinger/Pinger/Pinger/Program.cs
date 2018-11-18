1914e5c4442550c450cdb802d47e910fc12adb6b
Илья Кошманов <k0shmanov@yandex.ru>
2018-11-18 15:51:03 +0300

    Pinger alfa 0.0.0.2

--- CSharp/Pinger/Pinger/Pinger/Program.cs
+++ CSharp/Pinger/Pinger/Pinger/Program.cs
@@ -15,12 +15,26 @@ namespace Pinger
         static void Main(string[] args)
         {
             //переработай имена в более корректные и убодные не будь ленивой задницей!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            Ping Pinger = new Ping();
-            String addressHost = "ya.ru";//создание отдельного класса, тянуть из файла или массива данных???????? подумать как сделать лучше
-            int timeoutHost = 10000;
+            //добавь ведение лога с привязкой времени 
+            //последним добавить отправку сообщений на эл.почту (продумать как правильно сделать, чтобы не спамить)
+           
+            String inputHostName = "ya.ru";//создание отдельного класса, тянуть из файла или массива данных???????? подумать как сделать лучше
+            int  inputHostTimeoute = 10000;
+            PingOutput StartingAnalyze = new PingOutput();
+            StartingAnalyze.CreateTableHost(inputHostName, inputHostTimeoute);
 
-                //в последующем сделать методом, чтобы вызывать для каждого адреса через цикл + добавить вывод не строчками, а таблицей, динамически создаваемой
-                for (; ; ) {
+               
+
+        }
+    }
+    //переработай имена, они очень плохи! возникает путаница со встроенным классом Ping
+    class PingOutput
+    {
+        public void CreateTableHost(String addressHost, int timeoutHost)
+        {
+            Ping Pinger = new Ping();
+            for (; ; )
+                {
                 //сделать потоки, один для вывода на экран сообщений, второй для выхода из цикла(нажатием кнопки, вводом сообщения...??????)
                 PingReply ReplyInputDataHost = Pinger.Send(addressHost, timeoutHost);
                 Console.WriteLine("DNS Address: {0}", addressHost);
@@ -28,9 +42,8 @@ namespace Pinger
                 Console.WriteLine("RoundTrip time: {0}", ReplyInputDataHost.RoundtripTime);   //выделение цветом таймаута + обработка исключений если сервер недоступен
                 Thread.Sleep(1200);
                 Console.Clear(); //посмотреть как сделать плавное удаление данных из консоли, чтобы не моргало
-
-
-            }
+                }
         }
     }
+
 }
