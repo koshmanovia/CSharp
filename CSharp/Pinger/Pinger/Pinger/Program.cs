c0920f39bfe621490d9ff922a27f4d3f0a2ca1fb
Илья Кошманов <k0shmanov@yandex.ru>
2018-11-20 22:52:26 +0300

    Crash Version Pinger. Remaster Tomorow

--- CSharp/Pinger/Pinger/Pinger/Program.cs
+++ CSharp/Pinger/Pinger/Pinger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,35 +15,43 @@ namespace Pinger
     {
         static void Main(string[] args)
         {
-            //переработай имена в более корректные и убодные не будь ленивой задницей!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            //добавь ведение лога с привязкой времени 
-            //последним добавить отправку сообщений на эл.почту (продумать как правильно сделать, чтобы не спамить)
-           
-            String inputHostName = "ya.ru";//создание отдельного класса, тянуть из файла или массива данных???????? подумать как сделать лучше
+            List<String> inputHostName = new List<String>();
+            inputHostName.Add("ya.ru");
+            inputHostName.Add("google.com");
+            inputHostName.Add("nalog.ru");
+            
             int  inputHostTimeoute = 10000;
-            PingOutput StartingAnalyze = new PingOutput();
-            StartingAnalyze.CreateTableHost(inputHostName, inputHostTimeoute);
-
-               
-
+            PingerOutput startingAnalyze = new PingerOutput();
+            for(int i = 0; i < inputHostName.Count; i++)//убрать отсюда этот массив и запихать в метод, прости GitHub что такой позор в тебя заливаю
+            {
+                String tempHostName = inputHostName[i];
+                startingAnalyze.CreateTableHost(tempHostName, inputHostTimeoute);
+            }
+                       
         }
-    }
-    //переработай имена, они очень плохи! возникает путаница со встроенным классом Ping
-    class PingOutput
+    }    
+    class PingerOutput
     {
-        public void CreateTableHost(String addressHost, int timeoutHost)
+        public void CreateTableHost(String addressHost, int timeoutHost)//сюда передавать надо массив и перебирать непосредственно тут
         {
             Ping Pinger = new Ping();
-            for (; ; )
-                {
-                //сделать потоки, один для вывода на экран сообщений, второй для выхода из цикла(нажатием кнопки, вводом сообщения...??????)
-                PingReply ReplyInputDataHost = Pinger.Send(addressHost, timeoutHost);
-                Console.WriteLine("|DNS Address| IP Address   |RoundTrip time|\n {0}"+"       {1}"+"    {2}", addressHost, ReplyInputDataHost.Address.ToString(), ReplyInputDataHost.RoundtripTime);
-                 //выделение цветом таймаута + обработка исключений если сервер недоступен + шрамотное построение таблицы
+            for ( ; ; )
+            {   
+                
+                PingReply ReplyInputDataHost = Pinger.Send(addressHost, timeoutHost);                               
+                Console.WriteLine("|    DNS  Address    |   IP Address   | RoundTrip time|"+ //таблицу переписать, шапка печатается каждый раз, ничего удивительного
+                    "\n {0}" + "       {1}"+"    {2}", addressHost, ReplyInputDataHost.Address.ToString(), ReplyInputDataHost.RoundtripTime);              
                 Thread.Sleep(1200);
-                Console.Clear(); //посмотреть как сделать плавное удаление данных из консоли, чтобы не моргало
-                }
+                Console.Clear(); 
+            }
         }
+        //написать метод вычленяющий длинну хоста и если она больше 20 символов то урезать с конца лишнее и забивать таблицу
     }
 
 }
+//переработай имена в более корректные и убодные не будь ленивой задницей, думай как их сделать более удобными постоянно!!!
+//добавь ведение лога с привязкой времени 
+//сделать потоки, один для вывода на экран сообщений, второй для выхода из цикла(нажатием кнопки, вводом сообщения...??????)
+//создание отдельного класса inputHostName, тянуть из файла, плюс отдельная прога для редакирования данных.
+//выделение цветом таймаута + обработка исключений если сервер недоступен + грамотное построение таблицы обязательно динамическое
+//последним добавить отправку сообщений на эл.почту (продумать как правильно сделать, чтобы не спамить)
